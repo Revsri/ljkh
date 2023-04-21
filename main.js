b6ee6f32ab43b89f7911143f0038abaaa9bcaec7
Revsri <117416155+Revsri@users.noreply.github.com>
2023-04-20 17:46:07 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,32 @@
+//https://teachablemachine.withgoogle.com/models/JYi9yHsJZ/
+
+function setup()
+{
+    canvas = createCanvas(300, 300);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.hide();
+    classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/JYi9yHsJZ/model.json',modelLoaded);
+}
+
+function modelLoaded()
+{
+    console.log('Model Loaded!');
+}
+
+function draw()
+{
+    image(video, 0, 0, 300, 300);
+    classifier.classify(video, gotResult);
+}
+
+function gotResult(error, results)
+{
+    if(error) {
+        console.error(error);
+    } else {
+        console.log(results);
+        document.getElementById("name").innerHTML = results[0].label;
+        document.getElementById("accuracy").innerHTML = results[0].confidence * 100;
+    }
+}
